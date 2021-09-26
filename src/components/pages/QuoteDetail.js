173c92f2619b5868e17a0e01c64983bcdc24877b
L1ndseyHerman <lherman21@jcu.edu>
2021-09-26 19:56:13 -0400

    Caught up to instructor's vid276 code and tried his vid277 code myself.

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,14 +1,20 @@
+import { Fragment } from 'react';
 import {useParams} from 'react-router-dom';
+import { Route } from "react-router-dom";
+
 
 const QuoteDetail = () => {
 
     //  returns a params object
     const params = useParams();
 
-    return <section>
+    return <Fragment>
         <h1>Quote Detail Page</h1>
         <p>{params.quoteId}</p>
-    </section>;
+        <Route path="/quotes/:quoteId/comments">
+            <p>Placeholder Text</p>
+        </Route>
+    </Fragment>;
 };
 
 export default QuoteDetail;
