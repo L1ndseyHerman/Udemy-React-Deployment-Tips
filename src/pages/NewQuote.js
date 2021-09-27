9ddc451e8c877e8976ed61fa83022179a77ee73e
L1ndseyHerman <lherman21@jcu.edu>
2021-09-27 19:14:24 -0400

    Caught up to vid282: using the useHistory() url stack to go to the pushed url.

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,19 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
 
+    //  History is a stack that adds or removes urls. Can .push() or .replace() (the current page).
+    const history = useHistory();
+
     const addQuoteHandler = quoteData => {
         console.log(quoteData);
+
+        history.push('/quotes');
     };
 
-    return <QuoteForm onAddProp={addQuoteHandler} />;
+    return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
